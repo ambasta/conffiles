9d96619fc2aa7267418ebe3b04b748fbc26264b1
Amit Prakash Ambasta <amit.prakash.ambasta@gmail.com>
2021-01-06 12:56:02 +0530

    Add install scripts for homedir, etccon

---
+++ etc_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+for file in $(find etc -type f)
+do
+    filepath="/$file"
+    filedir=$(dirname $filepath)
+
+    if !( [ -d "$filedir" ] )
+    then
+        mkdir -p $filedir
+    fi
+
+    if [ -f "$filepath" ] || [ -L "$filepath" ]
+    then
+        rm $filepath
+    fi
+    ln -s $(pwd)/${file} $filepath
+done
---
+++ user_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+for file in $(find homedir -type f)
+do
+    filepath=$HOME/${file#homedir/}
+    filedir=$(dirname $filepath)
+
+    if ! ( [ -d "$filedir" ] && [ $filedir != $HOME ] )
+    then
+        mkdir -p $filedir
+    fi
+
+    if [ -f "$filepath" ] | [ -L "$filepath" ]
+    then
+        rm "$filepath"
+    fi
+    ln -s $(pwd)/${file} $filepath
+done
