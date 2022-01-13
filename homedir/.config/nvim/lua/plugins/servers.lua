a83c2f4f5745bf91b62aed619a10bcaead41e26e
Amit Prakash Ambasta <amit.prakash.ambasta@gmail.com>
2022-01-13 09:47:43 +0530

    Update nvim, gentoo-sources patch, patch for waybar

--- homedir/.config/nvim/lua/plugins/servers.lua
+++ homedir/.config/nvim/lua/plugins/servers.lua
@@ -4,6 +4,7 @@ local capabilities = require("plugins.capabilities")
 local lspconfig_util = require("lspconfig.util")
 
 local servers = {
+  "angularls",
 	"clangd",
 	"cmake",
 	"eslint",
@@ -35,6 +36,15 @@ for _, server in ipairs(servers) do
 		local default_opts = require("lspconfig.server_configurations.jsonls")
 		opts.cmd = vim.list_extend({ "yarn" }, default_opts.default_config.cmd)
 		opts.root_dir = lspconfig_util.root_pattern(".yarn")
+	elseif server == "angularls" then
+		local default_opts = require("lspconfig.server_configurations.angularls")
+    local cmd = {"yarn", "ngserver", "--stdio", "--tsProbeLocations", "./", "--ngProbeLocations", "./"}
+    -- opts.cmd = vim.list_extend({ "yarn" }, default_opts.default_config.cmd)
+    opts.cmd = cmd
+		opts.root_dir = lspconfig_util.root_pattern("angular.json")
+    opts.on_new_config = function(new_config, new_root_dir)
+      new_config.cmd = opts.cmd
+    end
 	elseif server == "pyright" then
 		opts.on_attach = utils.on_attach_noformat
 	elseif server == "jsonls" then
