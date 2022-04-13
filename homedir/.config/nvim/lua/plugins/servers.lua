390b2b5c974c727af6bb824875bd8870899e8aae
Amit Prakash Ambasta <amit.prakash.ambasta@gmail.com>
2022-04-13 12:13:45 +0530

    servers.lua: Linting

--- homedir/.config/nvim/lua/plugins/servers.lua
+++ homedir/.config/nvim/lua/plugins/servers.lua
@@ -5,15 +5,14 @@ local lspconfig_util = require("lspconfig.util")
 
 local servers = {
   "angularls",
-	"clangd",
-	"cmake",
-	"eslint",
-	"jsonls",
-	"gopls",
-	"pyright",
-	"rls",
-	"tsserver",
-  "jdtls"
+  "clangd",
+  "cmake",
+  "eslint",
+  "jsonls",
+  "gopls",
+  "pyright",
+  "rls",
+  "tsserver"
 }
 
 function GetFileMatchingPattern(search_dir, pattern)
@@ -28,44 +27,48 @@ function GetFileMatchingPattern(search_dir, pattern)
 end
 
 for _, server in ipairs(servers) do
-	local opts = {
-		on_attach = utils.on_attach,
-		capabilities = capabilities.capabilities,
-		flags = {
-			debounce_text_changes = 150,
-		},
-	}
+  local opts = {
+    on_attach = utils.on_attach,
+    capabilities = capabilities.capabilities,
+    flags = {
+      debounce_text_changes = 150,
+    },
+  }
 
-	if server == "tsserver" then
-		local default_opts = require("lspconfig.server_configurations.tsserver")
-		opts.cmd = vim.list_extend({ "yarn" }, default_opts.default_config.cmd)
+  if server == "tsserver" then
+    local default_opts = require("lspconfig.server_configurations.tsserver")
+    opts.cmd = vim.list_extend({ "yarn" }, default_opts.default_config.cmd)
     opts.root_dir = lspconfig_util.root_pattern({".yarn", "node_modules"})
-	elseif server == "eslint" then
-		local default_opts = require("lspconfig.server_configurations.eslint")
-		opts.cmd = vim.list_extend({ "yarn" }, default_opts.default_config.cmd)
+  elseif server == "eslint" then
+    local default_opts = require("lspconfig.server_configurations.eslint")
+    opts.cmd = vim.list_extend({ "yarn" }, default_opts.default_config.cmd)
     opts.root_dir = lspconfig_util.root_pattern({".yarn", "node_modules"})
-	elseif server == "jsonls" then
-		local default_opts = require("lspconfig.server_configurations.jsonls")
-		opts.cmd = vim.list_extend({ "yarn" }, default_opts.default_config.cmd)
+    opts.settings = {
+      format = true,
+      packageManager = "yarn"
+    }
+  elseif server == "jsonls" then
+    local default_opts = require("lspconfig.server_configurations.jsonls")
+    opts.cmd = vim.list_extend({ "yarn" }, default_opts.default_config.cmd)
     opts.root_dir = lspconfig_util.root_pattern({".yarn", "node_modules"})
-	elseif server == "angularls" then
-		local default_opts = require("lspconfig.server_configurations.angularls")
+  elseif server == "angularls" then
+    local default_opts = require("lspconfig.server_configurations.angularls")
     local cmd = {"yarn", "ngserver", "--stdio", "--tsProbeLocations", "./", "--ngProbeLocations", "./"}
     opts.cmd = cmd
-		opts.root_dir = lspconfig_util.root_pattern("angular.json")
+    opts.root_dir = lspconfig_util.root_pattern("angular.json")
     opts.on_new_config = function(new_config, new_root_dir)
       new_config.cmd = opts.cmd
     end
-	elseif server == "pyright" then
-		opts.on_attach = utils.on_attach_noformat
-	elseif server == "jsonls" then
-		opts.commands = {
-			Format = {
-				function()
-					vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line("$"), 0 })
-				end,
-			},
-		}
+  elseif server == "pyright" then
+    opts.on_attach = utils.on_attach_noformat
+  elseif server == "jsonls" then
+    opts.commands = {
+      Format = {
+        function()
+          vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line("$"), 0 })
+        end,
+      },
+    }
   elseif server == "jdtls" then
     local root_dir = lspconfig_util.root_pattern({".git", "mvnw", "gradlew", "pom.xml"})
     local home = os.getenv('HOME')
@@ -140,14 +143,14 @@ for _, server in ipairs(servers) do
         }
       }
     }
-	end
+  end
 
-	handler[server].setup(opts)
-	vim.cmd([[ do User LspAttachBuffers ]])
+  handler[server].setup(opts)
+  vim.cmd([[ do User LspAttachBuffers ]])
 end
 
 local exports = {
-	handler = handler,
+  handler = handler,
 }
 
 return exports
