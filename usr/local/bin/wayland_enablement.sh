f6785394417aa70c130ecf65a922267917d03a98
Amit Prakash Ambasta <amit.prakash.ambasta@gmail.com>
2022-08-10 10:11:31 +0530

    Updated conffiles for useflags, sway set, plugins, and wayland enablement

--- usr/local/bin/wayland_enablement.sh
+++ usr/local/bin/wayland_enablement.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-export CLUTTER_BACKEND=wayland
-export EGL_PLATFORM=wayland
-export GDK_BACKEND=wayland
-export GDK_GL=gles
+# export CLUTTER_BACKEND=wayland
+# export EGL_PLATFORM=wayland
+# export GDK_BACKEND=wayland
+# export GDK_GL=gles
 # export MESA_GLSL_CACHE_DISABLE=true
-export DISABLE_RTKIT=1
-export MOZ_ENABLE_WAYLAND=1
-export MOZ_GTK_TITLEBAR_DECORATION=client
-export NO_AT_BRIDGE=1
+# export DISABLE_RTKIT=1
+# export MOZ_ENABLE_WAYLAND=1
+# export MOZ_GTK_TITLEBAR_DECORATION=client
+
+# Enable if we see "Couldn't register with accessibility" bus in logs
+# export NO_AT_BRIDGE=1
 export SDL_VIDEODRIVER=wayland
-export VDPAU_DRIVER=radeonsi
+export _JAVA_AWT_WM_NONREPARENTING=1
+# export VDPAU_DRIVER=radeonsi
 export XKB_DEFAULT_LAYOUT=us
 export WLR_RENDERER=vulkan
