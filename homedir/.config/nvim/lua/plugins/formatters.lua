f38e21b10ee946a8f4e67198751e3fae288589f1
Amit Prakash Ambasta <amit.prakash.ambasta@gmail.com>
2022-10-07 11:50:11 +0530

    Signed-off-by: Amit Prakash Ambasta <amit.prakash.ambasta@gmail.com>
    package.accept_keywords: Unkeyworded all packages, gcc/clang now both stable in portage tree
    package.use: Useflags for clang-common, pgo enabled on firefox
    sets/editors: Removed pynvim
    sets/sway: Added swaylock
    .gitconfig: Auto-setup remote
    nvim/plugins: Updated to remote

--- homedir/.config/nvim/lua/plugins/formatters.lua
+++ homedir/.config/nvim/lua/plugins/formatters.lua
@@ -1,19 +1,24 @@
 local handler = require("null-ls")
+local resolver = require("null-ls.helpers.command_resolver")
+local utils = require("common.utils")
 
 handler.setup({
-  sources = {
-    handler.builtins.formatting.stylua,
-    -- handler.builtins.formatting.prettier.with({
-    --   command = "yarn",
-    --   args = { "run", "-B", "prettier", "--stdin-filepath", "$FILENAME" }
-    -- }),
-    handler.builtins.formatting.black,
-    handler.builtins.formatting.cmake_format,
-  }
+	sources = {
+		handler.builtins.formatting.black,
+		handler.builtins.formatting.cmake_format,
+		handler.builtins.formatting.isort,
+		handler.builtins.formatting.prettierd.with({
+			dynamic_command = function(params)
+				return resolver.from_yarn_pnp(params)
+			end,
+		}),
+		handler.builtins.formatting.stylua,
+	},
+	on_attach = utils.on_attach,
 })
 
 local exports = {
-  handler = handler,
+	handler = handler,
 }
 
 return exports
--- homedir/.config/nvim/lua/plugins/servers.lua
+++ homedir/.config/nvim/lua/plugins/servers.lua
@@ -4,165 +4,166 @@ local capabilities = require("plugins.capabilities")
 local lspconfig_util = require("lspconfig.util")
 
 local servers = {
-  "angularls",
-  "clangd",
-  "cmake",
-  "eslint",
-  "jdtls",
-  "jsonls",
-  "gopls",
-  "pyright",
-  "rust_analyzer",
-  "tsserver"
+	"angularls",
+	"clangd",
+	"cmake",
+	"eslint",
+	"jdtls",
+	"jsonls",
+	"gopls",
+	"pyright",
+	"rust_analyzer",
+	"tsserver",
 }
 
 function GetFileMatchingPattern(search_dir, pattern)
-  local files = vim.fn.glob(search_dir .. "/**/" .. pattern, false, true)
+	local files = vim.fn.glob(search_dir .. "/**/" .. pattern, false, true)
 
-  if vim.tbl_isempty(files) then
-    error("Searched files: " .. search_dir .. "/**/" .. pattern .. " : " .. vim.inspect(files))
-    error("No files found matching pattern: " .. pattern)
-  end
+	if vim.tbl_isempty(files) then
+		error("Searched files: " .. search_dir .. "/**/" .. pattern .. " : " .. vim.inspect(files))
+		error("No files found matching pattern: " .. pattern)
+	end
 
-  return files[1]
+	return files[1]
 end
 
 for _, server in ipairs(servers) do
-  local opts = {
-    on_attach = utils.on_attach,
-    capabilities = capabilities.capabilities,
-    flags = {
-      debounce_text_changes = 150,
-    },
-  }
+	local opts = {
+		on_attach = utils.on_attach,
+		capabilities = capabilities.capabilities,
+		flags = {
+			debounce_text_changes = 150,
+		},
+	}
 
-  if server == "tsserver" then
-    local default_opts = require("lspconfig.server_configurations.tsserver")
-    opts.cmd = vim.list_extend({ "yarn" }, default_opts.default_config.cmd)
-    opts.root_dir = lspconfig_util.root_pattern({".yarn", "node_modules"})
-  elseif server == "eslint" then
-    local default_opts = require("lspconfig.server_configurations.eslint")
-    opts.cmd = vim.list_extend({ "yarn" }, default_opts.default_config.cmd)
-    opts.root_dir = lspconfig_util.root_pattern({".yarn", "node_modules"})
-    opts.settings = {
-      format = true,
-      packageManager = "yarn"
-    }
-  elseif server == "jsonls" then
-    local default_opts = require("lspconfig.server_configurations.jsonls")
-    opts.cmd = vim.list_extend({ "yarn" }, default_opts.default_config.cmd)
-    opts.root_dir = lspconfig_util.root_pattern({".yarn", "node_modules"})
-  elseif server == "angularls" then
-    local default_opts = require("lspconfig.server_configurations.angularls")
-    local cmd = {"yarn", "ngserver", "--stdio", "--tsProbeLocations", "./", "--ngProbeLocations", "./"}
-    opts.cmd = cmd
-    opts.root_dir = lspconfig_util.root_pattern("angular.json")
-    opts.on_new_config = function(new_config, new_root_dir)
-      new_config.cmd = opts.cmd
-    end
-  elseif server == "pyright" then
-    opts.on_attach = utils.on_attach_noformat
-  elseif server == "jsonls" then
-    opts.commands = {
-      Format = {
-        function()
-          vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line("$"), 0 })
-        end,
-      },
-    }
-  elseif server == "rust_analyzer" then
-    opts.settings = {
-      rust = {
-        unstable_features = true,
-        build_on_save = false,
-        all_features = true,
-      },
+	if server == "tsserver" then
+		local default_opts = require("lspconfig.server_configurations.tsserver")
+		opts.cmd = vim.list_extend({ "yarn" }, default_opts.default_config.cmd)
+		opts.root_dir = lspconfig_util.root_pattern({ ".yarn", "node_modules" })
+	elseif server == "eslint" then
+		local default_opts = require("lspconfig.server_configurations.eslint")
+		opts.cmd = vim.list_extend({ "yarn" }, default_opts.default_config.cmd)
+		opts.root_dir = lspconfig_util.root_pattern({ ".yarn", "node_modules" })
+		opts.settings = {
+			validate = "on",
+			packageManager = "yarn",
+			format = true,
+			quiet = false,
+		}
+	elseif server == "jsonls" then
+		local default_opts = require("lspconfig.server_configurations.jsonls")
+		opts.cmd = vim.list_extend({ "yarn" }, default_opts.default_config.cmd)
+		opts.root_dir = lspconfig_util.root_pattern({ ".yarn", "node_modules" })
+	elseif server == "angularls" then
+		local default_opts = require("lspconfig.server_configurations.angularls")
+		local cmd = { "yarn", "ngserver", "--stdio", "--tsProbeLocations", "./", "--ngProbeLocations", "./" }
+		opts.cmd = cmd
+		opts.root_dir = lspconfig_util.root_pattern("angular.json")
+		opts.on_new_config = function(new_config, new_root_dir)
+			new_config.cmd = opts.cmd
+		end
+	elseif server == "pyright" then
+		opts.on_attach = utils.on_attach
+	elseif server == "jsonls" then
+		opts.commands = {
+			Format = {
+				function()
+					vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line("$"), 0 })
+				end,
+			},
+		}
+	elseif server == "rust_analyzer" then
+		opts.settings = {
+			rust = {
+				unstable_features = true,
+				build_on_save = false,
+				all_features = true,
+			},
+		}
+	elseif server == "jdtls" then
+		local root_dir = lspconfig_util.root_pattern({ ".git", "mvnw", "gradlew", "pom.xml" })
+		local home = os.getenv("HOME")
+		local workspace_dir = home .. "/.workspace/"
+		-- local jdls = GetFileMatchingPattern(home .. "/.local/opt", "jdt-language-server-*.jar")
+		-- local lombok = GetFileMatchingPattern(home  .. "/.local/opt", "lombok-*.jar");
+		local equinox = GetFileMatchingPattern(home .. "/.local/opt", "org.eclipse.equinox.launcher_*.jar")
+		local configdir = GetFileMatchingPattern(home .. "/.local/opt", "config_linux")
 
-    }
-  elseif server == "jdtls" then
-    local root_dir = lspconfig_util.root_pattern({".git", "mvnw", "gradlew", "pom.xml"})
-    local home = os.getenv('HOME')
-    local workspace_dir = home .. "/.workspace/"
-    -- local jdls = GetFileMatchingPattern(home .. "/.local/opt", "jdt-language-server-*.jar")
-    -- local lombok = GetFileMatchingPattern(home  .. "/.local/opt", "lombok-*.jar");
-    local equinox = GetFileMatchingPattern(home .. "/.local/opt", "org.eclipse.equinox.launcher_*.jar");
-    local configdir = GetFileMatchingPattern(home .. "/.local/opt", "config_linux")
+		opts.cmd = {
+			"java",
+			"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+			"-Dosgi.bundles.defaultStartLevel=4",
+			"-Declipse.product=org.eclipse.jdt.ls.core.product",
+			"-Dlog.protocol=true",
+			"-Dlog.level=ALL",
+			-- '-javaagent:'.. lombok,
+			"-Xms1g",
+			"-Xmx2G",
+			"-jar",
+			-- jdls,
+			equinox,
+			"-configuration",
+			configdir,
+			"--add-modules=ALL-SYSTEM",
+			"--add-opens",
+			"java.base/java.util=ALL-UNNAMED",
+			"--add-opens",
+			"java.base/java.lang=ALL-UNNAMED",
+			"-data",
+			workspace_dir,
+		}
+		opts.flags = {
+			allow_incremental_sync = true,
+		}
+		opts.capabilities.workspace.configuration = true
+		opts.capabilities.textDocument = {
+			completion = {
+				completionItem = {
+					snippetSupport = true,
+				},
+			},
+		}
+		opts.root_dir = root_dir
+		opts.settings = {
+			java = {
+				autobuild = {
+					enabled = true,
+				},
+				import = {
+					generatesMetadataFilesAtProjectRoot = true,
+				},
+				format = {
+					settings = {
+						url = "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml",
+					},
+				},
+				sources = {
+					organizeImports = {
+						starThreshold = 9999,
+						staticStarThreshold = 9999,
+					},
+				},
+				codeGeneration = {
+					toString = {
+						template = "$(object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+					},
+				},
+				saveActions = {
+					organizeImports = true,
+				},
+				trace = {
+					server = "verbose",
+				},
+			},
+		}
+	end
 
-    opts.cmd = {
-      'java',
-      '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-      '-Dosgi.bundles.defaultStartLevel=4',
-      '-Declipse.product=org.eclipse.jdt.ls.core.product',
-      '-Dlog.protocol=true',
-      '-Dlog.level=ALL',
-      -- '-javaagent:'.. lombok,
-      '-Xms1g',
-      '-Xmx2G',
-      '-jar',
-      -- jdls,
-      equinox,
-      '-configuration',
-      configdir,
-      '--add-modules=ALL-SYSTEM',
-      '--add-opens',
-      'java.base/java.util=ALL-UNNAMED',
-      '--add-opens',
-      'java.base/java.lang=ALL-UNNAMED',
-      '-data',
-      workspace_dir,
-    }
-    opts.flags = {
-      allow_incremental_sync = true,
-    }
-    opts.capabilities.workspace.configuration = true
-    opts.capabilities.textDocument = {
-      completion = {
-        completionItem = {
-          snippetSupport = true
-        }
-      }
-    }
-    opts.root_dir = root_dir
-    opts.settings = {
-      java = {
-        autobuild = {
-          enabled = true
-        },
-        import = {
-          generatesMetadataFilesAtProjectRoot = true
-        },
-        format = {
-          settings = {
-            url = "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml"
-          }
-        },
-        sources = {
-          organizeImports = {
-            starThreshold = 9999;
-            staticStarThreshold = 9999;
-          }
-        },
-        codeGeneration = {
-          toString = {
-            template = "$(object.className}{${member.name()}=${member.value}, ${otherMembers}}"
-          }
-        },
-        saveActions = {
-          organizeImports = true
-        },
-        trace = {
-          server = "verbose"
-        }
-      }
-    }
-  end
-
-  handler[server].setup(opts)
-  vim.cmd([[ do User LspAttachBuffers ]])
+	handler[server].setup(opts)
+	vim.cmd([[ do User LspAttachBuffers ]])
 end
 
 local exports = {
-  handler = handler,
+	handler = handler,
 }
 
 return exports
