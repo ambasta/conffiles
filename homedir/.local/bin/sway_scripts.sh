0a0b87c5cb9e2a17061a1acf5a21955038b232e5
Amit Prakash Ambasta <amit.prakash.ambasta@gmail.com>
2023-10-21 22:11:01 +0530

    sway: added support for swayr, disabled local sway scripts

--- homedir/.local/bin/sway_scripts.sh
+++ homedir/.local/bin/sway_scripts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
+# systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+# dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
