a15ae3f942adcabac90e6f18dd833a0f5bf83a3d
Amit Prakash Ambasta <amit.prakash.ambasta@gmail.com>
2025-02-10 12:59:02 +0530

    Revised config

--- homedir/.bashrc
+++ homedir/.bashrc
@@ -1,6 +1,7 @@
 # export XDG_CONFIG_HOME=$HOME/.config
 # export XDG_CACHE_HOME=$HOME/.cache
 source /etc/profile
+export COREPACK_ENABLE_DOWNLOAD_PROMPT=0
 
 export PATH=$PATH:/home/amitprakash/.local/bin:$(yarn global bin)
 export SSH_AUTH_SOCK=/run/user/$(id -u)/gcr/ssh
@@ -9,6 +10,8 @@ alias awsshell='source /home/amitprakash/.virtualenvs/awsshell/bin/activate'
 alias pyshell='source /home/amitprakash/.virtualenvs/pyshell/bin/activate'
 
 # keychain ~/.ssh/id_rsa
+eval $(keychain --agents ssh --eval id_rsa)
+eval $(keychain --eval --agents gpg F0B27A3B6D7CF774)
 . ~/.keychain/${HOSTNAME}-sh
 . ~/.keychain/${HOSTNAME}-sh-gpg
 
@@ -30,3 +33,25 @@ export PATH=$PATH:/home/amitprakash/.cargo/bin
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
+
+eval $(systemctl --user show-environment | sed 's/^/export /')
+
+export PATH="$(yarn global bin):$PATH"
+
+source /home/amitprakash/.bash_completions/llm_benchmark.sh
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/amitprakash/conda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/amitprakash/conda/etc/profile.d/conda.sh" ]; then
+        . "/home/amitprakash/conda/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/amitprakash/conda/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
--- homedir/.config/nvim/lua/plugins/lspconfig.lua
+++ homedir/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,16 +1,65 @@
+local lspconfig_util = require("lspconfig.util")
+
 return {
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        clangd = { mason = false },
+        eslint = {
+          root_dir = function(startpath)
+            return vim.fs.dirname(vim.fs.find("yarn.lock", { path = startpath, upward = true })[1])
+          end,
+        },
+        clangd = {
+          mason = false,
+          cmd = {
+            "clangd",
+            "--background-index",
+            "--experimental-modules-support",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-style=llvm",
+          },
+        },
+        neocmake = {
+          cmd = { "neocmakelsp", "--stdio" },
+          single_file_support = true,
+          init_options = {
+            format = {
+              enable = true,
+            },
+            line = {
+              enable = true,
+            },
+          },
+        },
+        tsserver = {
+          enabled = false,
+        },
+        ts_ls = {
+          enabled = false,
+        },
         vtsls = {
+          root_dir = function(startpath)
+            return vim.fs.dirname(vim.fs.find("yarn.lock", { path = startpath, upward = true })[1])
+          end,
           settings = {
+            autoUseWorkspaceTsdk = true,
             typescript = {
               tsdk = "./.yarn/sdks/typescript/lib",
             },
           },
         },
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ["python"] = { "isort", "black" },
+        ["cmake"] = { "gersemi" },
       },
     },
   },
